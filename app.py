bd8f2cf263efba6899c31ddc79c1103ef25a0dc5
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-12-06 10:56:24 +0100

    update

---
+++ app.py
@@ -0,0 +1,56 @@
+#---------------------------------------------------printing----------------------------------------------------------------------
+
+print('Hello World, welcome')
+print('Welcome')
+print('My Name is Lili I am', 100, 'years')
+
+
+#---------------------------------------------------variables-----------------------------------------------------------------------
+
+name = 'Tim'
+
+"""
+name is the name of variable
+
+'Tim' is the value
+"""
+
+print(name)
+
+print(name + 'is a boy')
+print(name + 'is 18')
+print(name + 'is from Turkey')
+
+#-----------------------------------------------------data types--------------------------------------------------------------------
+
+
+
+name = 'Roy'
+age = 18
+
+print(name)
+print(name + 'is a boy')
+print(name,'is', age)
+
+
+"""
+string
+number/integer
+boolean
+"""
+
+# string
+print('Hi. How are you')
+print('Hi. \nHow are you')
+print('Hi. \'How are you\'') #using backslash to show a quote
+
+fruit = 'Banana'
+print(fruit[2]) #print letter at index 2
+print(fruit.upper())
+print(fruit.islower()) #false coz must all lowercase
+print(fruit.lower())
+print(fruit.isupper()) #false coz must all uppercase
+print(fruit.upper().isupper()) #true
+print(fruit) #Banana
+print(len(name)) #3
+
