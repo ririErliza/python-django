4b8c175a635006ddf520f5231225ceaa9a83bb6c
ririErliza <95483807+ririErliza@users.noreply.github.com>
2023-06-14 11:03:40 +0200

    Update tryExcept.py

--- tryExcept.py
+++ tryExcept.py
@@ -7,8 +7,8 @@
 #     print(x)
 # except:
 #     print('Something went wrong, that\'s not integer')
-#Input an integer : r
-#Something went wrong, that's not integer
+# #Input an integer : r
+# #Something went wrong, that's not integer
 
 try:
     x=int(input('Input an integer : '))
@@ -16,5 +16,26 @@ try:
 except ValueError:
     print('Value is not integer')
 
-# Input an integer : e
-# Value is not integer
+# # Input an integer : e
+# # Value is not integer
+
+try:
+    x=int(input('Input an integer : '))
+    print(x)
+except:
+    print('Not correct')
+else:
+    print('Correct!')
+
+# # Input an integer : e
+# # Not correct
+
+
+try:
+    x=int(input('Input an integer : '))
+    print(x)
+except:
+    print('Not correct')
+finally: # this will run either there is an error or not
+    print('TrymExcept Finished!')
+
